be1d2612782944bb2734600523c2fb7fb0fb695b
Harish <harishkumarsp023@gmail.com>
2024-06-13 09:10:29 +0530

    chore: Update layout.tsx to include favicon and head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,11 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { AuthProvider } from './context/AuthContext';
-
+import Head from 'next/head';
 const inter = Inter({ subsets: ['latin'] });
-
+import fivcon from './favicon.ico'
 export const metadata: Metadata = {
+  
   title: 'Kavasam Learning Academy',
   description: 'Developed by Harish',
 };
@@ -19,7 +20,9 @@ export default function RootLayout({
   return (
     <AuthProvider>
       <html lang="en">
+    
         <body className={inter.className}>
+        {/* <link rel="icon" href="./favicon.ico" sizes="any" /> */}
           {children}
         </body>
       </html>
